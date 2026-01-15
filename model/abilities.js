bfbba62ad948ddc320e45d18d9880d9fd7bdba82
AAshGray <193518051+AAshGray@users.noreply.github.com>
2026-01-15 09:08:56 -0800

    created initial sequelize models for database connectivity

---
+++ model/abilities.js
@@ -0,0 +1,42 @@
+import {DataTypes} from 'sequelize';
+
+export default (sequelize) => {
+    return sequelize.define('abilities', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        class_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'class_reference',
+                key: 'id',
+                ondelete: 'CASCADE'
+            }
+        },
+        ability_name: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        ability_description: {
+            type: DataTypes.TEXT
+        },
+        ability_type: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        level_required: {
+            type: DataTypes.INTEGER
+        },
+        prerequisite_required: DataTypes.Boolean,
+        default_value: {
+            type: Boolean,
+            defaultValue: false
+        },
+        prerequisite_list_id: {
+            type: DataTypes.INTEGER,
+        }
+    });
+}
---
+++ model/characters.js
@@ -0,0 +1,63 @@
+import {DataTypes} from 'sequelize';
+export default (sequelize) => {
+    return sequelize.define('characters', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'users',
+                key: 'id',
+                ondelete: 'CASCADE'
+            }
+        },
+        char_name: {
+            type: DataTypes.STRING(100),
+            allowNull: false
+        },
+        total_level: {
+            type: DataTypes.INTEGER,
+            defaultValue: 1
+        },
+        total_hp: {
+            type: DataTypes.INTEGER,
+            defaultValue: 1
+        },
+        initiative_bonus: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        strength: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10
+        },
+        dexterity: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10
+        },
+        constitution: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10
+        },
+        intelligence: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10
+        },
+        wisdom: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10
+        },
+        charisma: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10
+        },
+        languages: {
+            type: DataTypes.STRING(255),
+        }
+    });
+}
